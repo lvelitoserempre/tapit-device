406dc2ee663d30031a2e6ddaeea96af96067802e
Victor Rodriguez <vrodmz@gmail.com>
2020-04-09 13:39:19 -0500

    Delete register feature from the app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-import { HomeComponent } from './components/home/home.component';
-import { SignUpComponent } from './components/sign-up/sign-up/sign-up.component';
-import { AlreadyLoggedInGuard } from './guards/already-logged-in.guard';
-import { AuthGuard } from './guards/auth.guard';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LoginComponent} from './components/login/login.component';
+import {HomeComponent} from './components/home/home.component';
+import {AlreadyLoggedInGuard} from './guards/already-logged-in.guard';
+import {AuthGuard} from './guards/auth.guard';
 
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'home' },
-  { path: 'login', component: LoginComponent, canActivate: [ AlreadyLoggedInGuard ] },
-  { path: 'signup', component: SignUpComponent, canActivate: [ AlreadyLoggedInGuard ] },
-  { path: 'home', component: HomeComponent, canActivate: [ AuthGuard ] }
+  {path: '', pathMatch: 'full', redirectTo: 'home'},
+  {path: 'login', component: LoginComponent, canActivate: [AlreadyLoggedInGuard]},
+  {path: 'signup', redirectTo: 'login'},
+  {path: 'home', component: HomeComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
