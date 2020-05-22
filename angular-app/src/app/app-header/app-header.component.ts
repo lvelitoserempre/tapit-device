25f0c8721eff5a109d4d67bfe69a2f3f0cb437b0
Victor Rodriguez <vrodmz@gmail.com>
2020-05-22 11:52:46 -0500

    Header ready to go

--- angular-app/src/app/app-header/app-header.component.ts
+++ angular-app/src/app/app-header/app-header.component.ts
@@ -1,9 +1,7 @@
 import {ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
-import {LoaderService} from '../loader/loader-service/loader.service';
 import {UserAccount} from '../models/user-account.model';
 import {UserAuthenticationService} from '../user/user-authentication/user-authentication-service/user-authentication.service';
 import {Subscription} from 'rxjs';
-import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -14,12 +12,11 @@ export class AppHeaderComponent implements OnInit, OnDestroy {
   user: UserAccount;
   private userSubscription: Subscription;
 
-  constructor(private loaderService: LoaderService, private userService: UserAuthenticationService, private router: Router,
-              private changeDetectorRef: ChangeDetectorRef) {
+  constructor(private userAuthenticationService: UserAuthenticationService, private changeDetectorRef: ChangeDetectorRef) {
   }
 
   ngOnInit(): void {
-    this.userSubscription = this.userService.getCurrentUser()
+    this.userSubscription = this.userAuthenticationService.getCurrentUser()
       .subscribe(user => {
         this.user = user;
         this.changeDetectorRef.detectChanges();
@@ -29,17 +26,4 @@ export class AppHeaderComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.userSubscription.unsubscribe();
   }
-
-  logout() {
-    this.loaderService.show();
-    this.userService.logout()
-      .subscribe(() => {
-        this.loaderService.hide();
-        this.router.navigateByUrl('auth/login');
-      }, (error) => {
-        console.error(error);
-        this.loaderService.hide();
-      });
-  }
-
 }
--- angular-app/src/app/user/user-profile/user-profile.component.ts
+++ angular-app/src/app/user/user-profile/user-profile.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {UserAccount} from '../../models/user-account.model';
 import {UserAuthenticationService} from '../user-authentication/user-authentication-service/user-authentication.service';
+import {LoaderService} from '../../loader/loader-service/loader.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-user-profile',
@@ -10,11 +12,24 @@ import {UserAuthenticationService} from '../user-authentication/user-authenticat
 export class UserProfileComponent implements OnInit {
   user: UserAccount;
 
-  constructor(private authService: UserAuthenticationService) {
+  constructor(private loaderService: LoaderService, private userAuthenticationService: UserAuthenticationService,
+              private router: Router) {
   }
 
   ngOnInit(): void {
-    this.authService.getCurrentUser().subscribe(user => this.user = user);
+    this.userAuthenticationService.getCurrentUser().subscribe(user => this.user = user);
+  }
+
+  logout() {
+    this.loaderService.show();
+    this.userAuthenticationService.logout()
+      .subscribe(() => {
+        this.loaderService.hide();
+        this.router.navigateByUrl('auth/login');
+      }, (error) => {
+        console.error(error);
+        this.loaderService.hide();
+      });
   }
 
 }
