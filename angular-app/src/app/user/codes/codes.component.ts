fb963a0b92252334fa517a4a2766a63ea64a67f2
Arturo Hernandez Lopez <arhernandezm@ids.com.mx>
2020-06-01 12:10:01 -0500

    Replace the loading spinner with the loder service (#11)

--- angular-app/src/app/user/codes/codes.component.ts
+++ angular-app/src/app/user/codes/codes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CodeDAO } from './code-dao.service';
 import Code from './codes';
 import { Subscription } from 'rxjs';
+import {LoaderService} from 'src/app/loader/loader-service/loader.service';
 
 @Component({
   selector: 'app-codes',
@@ -11,16 +12,16 @@ import { Subscription } from 'rxjs';
 export class MyCodesComponent implements OnInit, OnDestroy {
   codesSubscription: Subscription;
   codes: Code[];
-  showSpinner: boolean = true;
 
-  constructor(private codeDAO: CodeDAO) { }
+  constructor(private codeDAO: CodeDAO, private loaderService: LoaderService,) { }
   ngOnDestroy(): void {
     this.codesSubscription.unsubscribe();
   }
 
   ngOnInit(): void {
+    this.loaderService.show();
     this.codesSubscription = this.codeDAO.getAll().subscribe((codes: Code[]) => {
-      this.showSpinner = false;
+      this.loaderService.hide();
       this.codes = codes;
     })
   }
