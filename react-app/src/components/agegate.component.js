5cbc82aa3f3e4d6c5a3076531cd800c5988af87f
GLOBANT\jeisson.garcia <jeisson.garcia@globant.com>
2020-06-05 15:49:01 -0500

    bug fixing age gate ANH001-784

--- react-app/src/components/agegate.component.js
+++ react-app/src/components/agegate.component.js
@@ -45,27 +45,29 @@ export default function Agegate(props) {
 
     return (
         <div id='agegate' className='agegate row center-sm middle-sm'>
-            <div className='agegate__content col-xs-12 col-lg-6 center-xs middle-xs row'>
-                <div>
-                    <p className="section__text">¡Hola!</p>
-                    <p className="section__text">Por favor ingresa tu fecha de nacimiento</p>
-                    <form className='agegate__form row center-xs middle-xs'>
-                        <input type="text" maxLength="2" placeholder="DD" id="day" />
-                        <input type="text" maxLength="2" placeholder="MM" id="month" />
-                        <input type="text" maxLength="4" placeholder="AAAA" id="year" />
-                        {
-                            adult == false?
-                                <p className="section__text col-xs-12">Debes ser mayor de 18 años para acceder</p>
-                            :
-                            adult == 'invalid'?
-                                <p className="section__text col-xs-12">Por favor ingresa una fecha válida</p>
-                            :
-                            null
-                        }
-                        <div className="col-xs-12 center-xs">
-                            <input className="section__button" type="button" onClick={()=>checkAge()} value="Enviar" />
-                        </div>
-                    </form>
+            <div className='container center-xs row'>
+                <div className='agegate__content col-xs-12 col-lg-7 center-xs middle-sm top-xs row'>
+                    <div>
+                        <p className="section__text">¡Hola!</p>
+                        <p className="section__text">Por favor ingresa tu fecha de nacimiento</p>
+                        <form className='agegate__form row center-xs middle-xs'>
+                            <input type="text" maxLength="2" placeholder="DD" id="day" />
+                            <input type="text" maxLength="2" placeholder="MM" id="month" />
+                            <input type="text" maxLength="4" placeholder="AAAA" id="year" />
+                            {
+                                adult == false?
+                                    <p className="section__text col-xs-12">Debes ser mayor de 18 años para acceder</p>
+                                :
+                                adult == 'invalid'?
+                                    <p className="section__text col-xs-12">Por favor ingresa una fecha válida</p>
+                                :
+                                null
+                            }
+                            <div className="col-xs-12 center-xs">
+                                <input className="section__button" type="button" onClick={()=>checkAge()} value="Enviar" />
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
