01ed938370ab92b31f9d0514bad134c2c4c3bddf
Victor Rodriguez <vrodmz@gmail.com>
2020-06-26 17:01:38 -0500

    Environment of production rolled back

--- angular-app/src/environments/environment.prod.ts
+++ angular-app/src/environments/environment.prod.ts
@@ -24,7 +24,7 @@ export const environment = {
       createUser: '/v1/sso/user'
     }
   },
-  marketUrl: 'https://dev-market.tapit.com.co/api/vtexid/pub/logout?scope=bavariacotest&returnUrl=https://dev-market.tapit.com.co',
+  marketUrl: 'https://market.tapit.com.co/api/vtexid/pub/logout?scope=bavariaco&returnUrl=https://market.tapit.com.co',
   googleAnalyticsId: 'UA-159252784-5',
   googleTagManagerId: 'GTM-MGL9B93'
 };
