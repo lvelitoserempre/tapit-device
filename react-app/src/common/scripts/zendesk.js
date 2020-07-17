d58616f8f42ca0bc14817136fb0a5f86e5c21fa8
GLOBANT\jeisson.garcia <jeisson.garcia@globant.com>
2020-07-17 11:13:46 -0500

    fill zendesk form data with user data

--- react-app/src/common/scripts/zendesk.js
+++ react-app/src/common/scripts/zendesk.js
@@ -1,7 +1,7 @@
 window.zESettings = {
   webWidget: {
     contactForm: {
-      attachments: false
+      attachments: true
     },
     color: { 
       theme: '#FF5005',
@@ -10,3 +10,29 @@ window.zESettings = {
     }
   }
 };
+var getCookie = function (key) {
+  var cookieValue = document.cookie.match(key + '=[^ ;]*');
+  if (cookieValue) {
+    return cookieValue[0].replace(key + '=', '');
+  }
+}
+var user_cookie;
+var user_desk = getCookie('loggedUser');
+var user_email, user_fullName;
+
+if (user_desk !== '' && user_desk !== undefined && user_desk !== null) {
+  user_cookie = JSON.parse(decodeURIComponent(user_desk));
+  user_email = user_cookie.email;
+  user_fullName = user_cookie.firstName + ' ' + user_cookie.lastName;
+}
+
+zE('webWidget', 'prefill', {
+  name: {
+    value: user_fullName,
+    readOnly: true 
+  },
+  email: {
+    value: user_email,
+    readOnly: true 
+  }
+});
