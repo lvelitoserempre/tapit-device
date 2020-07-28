3fbbf0445ac2db92333f2b58d85c6cf250a80d97
Victor Rodriguez <vrodmz@gmail.com>
2020-07-28 18:05:20 -0500

    merge wrong. Corrected

--- angular-app/src/app/single-sign-on/app/app.component.ts
+++ angular-app/src/app/single-sign-on/app/app.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../../user/user-authentication/user-authentication-service/auth.service';
 import {IframeCommunicatorService} from '../iframe-communicator.service';
-<<<<<<< HEAD:angular-app/src/app/single-sign-on/app/app.component.ts
 import {initializeApp} from 'firebase';
 import {environment} from '../../../environments/environment';
+import {TranslateService} from '@ngx-translate/core';
+import {I18nService} from '../i18n.service';
 
 declare var zE;
-=======
-import { TranslateService } from '@ngx-translate/core';
-import { I18nService } from '../i18n.service';
->>>>>>> 35bb49e3c7f2110a59e6cb3b804fb48eda182b3e:angular-app/src/app/single-sign-on/single-sign-on/single-sign-on.component.ts
 
 @Component({
   selector: 'app-root',
@@ -19,7 +16,7 @@ import { I18nService } from '../i18n.service';
 export class AppComponent implements OnInit {
 
   constructor(private authService: AuthService, private iframeCommunicatorService: IframeCommunicatorService,
-    private i18n: I18nService, public translate: TranslateService) {
+              private i18n: I18nService, public translate: TranslateService) {
     this.translate.setDefaultLang(this.i18n.getCurrentLanguage());
   }
 
