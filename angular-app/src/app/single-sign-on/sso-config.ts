48e8fac5d36140df48dae5b3c0240bbf7dd600f0
victor <vrodmz@gmail.com>
2020-07-30 10:02:34 -0500

    Added config to hide the close button

--- angular-app/src/app/single-sign-on/sso-config.ts
+++ angular-app/src/app/single-sign-on/sso-config.ts
@@ -2,11 +2,12 @@ export default interface SSOConfig {
   origin: string;
   logoUrl: string;
   slogan: string;
-  showFacebookButton: boolean,
-  showOffersOption: boolean,
-  email: string,
-  emailHint: string,
-  passwordHint: string,
+  showFacebookButton: boolean;
+  showOffersOption: boolean;
+  email: string;
+  emailHint: string;
+  passwordHint: string;
   signUpLegalHtml: string;
   loginLegalHtml: string;
+  showCloseButton: boolean;
 }
