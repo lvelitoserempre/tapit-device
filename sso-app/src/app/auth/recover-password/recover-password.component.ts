6a88380d8c056b45ae5da5daf0cc4f1e4e7fd54b
GLOBANT\angel.herrera <angel.herrera@globant.com>
2020-08-11 16:49:14 -0500

    sso-brahma: recover-password component

---
+++ sso-app/src/app/auth/recover-password/recover-password.component.ts
@@ -0,0 +1,60 @@
+import {Component, OnInit} from '@angular/core';
+import {LoaderService} from '../../loader/loader-service/loader.service';
+import {DialogService} from '../../dialog/dialog-service/dialog.service';
+import {FacebookService} from '../facebook.service';
+import {UserDAO} from '../../user/user-dao.service';
+import {auth} from 'firebase';
+import {from} from 'rxjs';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {IframeMessagingService} from '../../shared/services/iframe-messaging.service';
+import SSOConfig from '../../single-sign-on/sso-config';
+import {SSOConfigService} from '../../single-sign-on/sso-config.service';
+import {Router} from '@angular/router';
+import { RecoverPasswordValidationMessages, RecoverPasswordValidators } from './recover-password.validations';
+
+
+@Component({
+  selector: 'app-recover-password',
+  templateUrl: './recover-password.component.html',
+  styleUrls: ['./recover-password.component.scss']
+})
+export class RecoverPasswordComponent implements OnInit {
+
+  recoverPasswordForm: FormGroup;
+  validationMessages = RecoverPasswordValidationMessages;
+  config: SSOConfig;
+  emailSent = false;
+
+  constructor(private loaderService: LoaderService, private dialogService: DialogService, private formBuilder: FormBuilder, 
+              private configService: SSOConfigService, private router: Router) {
+    this.recoverPasswordForm = this.formBuilder.group(RecoverPasswordValidators, {updateOn: 'blur'});
+  }
+
+  ngOnInit(): void {
+    this.configService.getConfig().subscribe(config => {
+      this.config = config;
+    });
+  }
+
+  recoverPassword() {
+    this.recoverPasswordForm.markAllAsTouched();
+
+    if (this.recoverPasswordForm.valid) {
+      const formValue = this.recoverPasswordForm.value;
+      this.loaderService.show();
+      from(auth().sendPasswordResetEmail(formValue.email))
+        .subscribe(resp => {
+          this.emailSent = true;
+          this.loaderService.hide();
+        }, error => {
+          this.loaderService.hide();
+          this.dialogService.manageError(error);
+        });
+    }
+  }
+
+  returnToLogin(){
+    this.router.navigateByUrl('login');
+  }
+
+}
